231deaeb977fabbf89cafd422064e24e05907630
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2023-11-13 17:49:25 +0900

    gloval_var 파일 수정

--- gloval_var.php
+++ gloval_var.php
@@ -1,7 +1,6 @@
 <?php 
 
 define("_HOST_", "13.125.36.197");  //Live
-// define("_HOST_", "rds-an2-inside-dev-go-newto-maria.czyj3edktxic.ap-northeast-2.rds.amazonaws.com"); //test
 define("_USER_", "Newto_1");
 define("_PWD_", "newto123");
 define("_DB_", "photoDB");
