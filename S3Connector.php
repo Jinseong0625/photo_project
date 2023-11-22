fdebd6b76c8f187fc33f889e210df86c5a2775d3
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2023-11-22 16:07:47 +0900

    Update S3Connector.php

--- S3Connector.php
+++ S3Connector.php
@@ -16,8 +16,8 @@ class S3Connector {
             'version'     => 'latest',
             'region'      => 'ap-northeast-2', // 예: us-east-1
             'credentials' => [
-                'key'    => AWS_ACCESS_KEY,
-                'secret' => AWS_SECRET_KEY,
+                'key'    => 'AKIAX73O662BZXU6ZJGY',
+                'secret' => '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',
             ],
         ]);
 
