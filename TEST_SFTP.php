c724c3d7f3c638c622cbb7e03f85a147063d3b92
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2023-11-29 10:44:55 +0900

    임시 테스트

--- TEST_SFTP.php
+++ TEST_SFTP.php
@@ -3,47 +3,51 @@
 #use phpseclib\Net\SFTP;
 use phpseclib3\Net\SFTP;
 use phpseclib3\Crypt\RSA;
+use phpseclib3\Crypt\PublicKeyLoader;
 
 require __DIR__ . '/vendor/autoload.php'; // Composer autoload 파일
+require __DIR__ . '/global_var.php';
 
 // SFTP 연결 정보
 $sftpHost = _HOST_;
 $sftpPort = 22;
 $sftpUsername = SFTP_NAME;
-$pemFilePath = '/home/ubuntu/FirstTestKey.pem';
+$pemFilePath = 'C:/Users/chomk/Downloads/FirstTestKey.pem';
 
 // SFTP 객체 생성
 $sftp = new SFTP($sftpHost, $sftpPort);
 
-// RSA 객체 생성 및 키 로드
-$key = new RSA();
-$key->load(file_get_contents($pemFilePath));
+// RSA 객체 생성 및 키 로드1
+#$key = new RSA();
+#$key->load(file_get_contents($pemFilePath));
+
+$key = PublicKeyLoader::load(file_get_contents($pemFilePath));
 
 // SFTP 연결
-if (!$sftp->login($sftpUsername, $Key)) {
+if (!$sftp->login($sftpUsername, $key)) {
     exit('Login Failed');
 }
 
 // 업로드
-$localFile = 'C:/Users/chomk/Downloads/수정사항.png';
-$remoteFile = '/home/ubuntu/수정사항.png';
+$localFile = 'C:/Users/chomk/Downloads/image.png';
+$remoteFile = '/home/ubuntu/image.png';
 
 if ($sftp->put($remoteFile, $localFile, SFTP::SOURCE_LOCAL_FILE)) {
     echo "File uploaded successfully.\n";
 } else {
     echo "File upload failed.\n";
 }
-
+/*
 // 다운로드
-$localDownloadFile = 'C:/Users/chomk/Downloads/수정사항.png';
-$remoteDownloadFile = '/home/ubuntu/수정사항.png';
+$localDownloadFile = 'C:/Users/chomk/Downloads/image.png';
+$remoteDownloadFile = '/home/ubuntu/image.png';
 
 if ($sftp->get($remoteDownloadFile, $localDownloadFile)) {
     echo "File downloaded successfully.\n";
 } else {
     echo "File download failed.\n";
 }
-
+*/
 // SFTP 연결 닫기
 $sftp->disconnect();
 
