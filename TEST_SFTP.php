d89ce971452c06300a38bad9e72fc251c858df74
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2023-11-29 11:09:48 +0900

    테스트 코드 수정

--- TEST_SFTP.php
+++ TEST_SFTP.php
@@ -8,6 +8,25 @@ use phpseclib3\Crypt\PublicKeyLoader;
 require __DIR__ . '/vendor/autoload.php'; // Composer autoload 파일
 require __DIR__ . '/global_var.php';
 
+// 업로드, 다운로드 함수 적용시
+function uploadFile(SFTP $sftp, $localFilePath, $remoteFilePath)
+{
+    if ($sftp->put($remoteFilePath, $localFilePath, SFTP::SOURCE_LOCAL_FILE)) {
+        echo "File uploaded successfully.\n";
+    } else {
+        echo "File upload failed.\n";
+    }
+}
+
+function downloadFile(SFTP $sftp, $remoteFilePath, $localFilePath)
+{
+    if ($sftp->get($remoteFilePath, $localFilePath)) {
+        echo "File downloaded successfully.\n";
+    } else {
+        echo "File download failed.\n";
+    }
+}
+
 // SFTP 연결 정보
 $sftpHost = _HOST_;
 $sftpPort = 22;
@@ -31,12 +50,14 @@ if (!$sftp->login($sftpUsername, $key)) {
 // 업로드
 $localFile = 'C:/Users/chomk/Downloads/image.png';
 $remoteFile = '/home/ubuntu/image.png';
+uploadFile($sftp, $localFileForUpload, $remoteFileForUpload);
 
-if ($sftp->put($remoteFile, $localFile, SFTP::SOURCE_LOCAL_FILE)) {
+/*if ($sftp->put($remoteFile, $localFile, SFTP::SOURCE_LOCAL_FILE)) {
     echo "File uploaded successfully.\n";
 } else {
     echo "File upload failed.\n";
-}
+} */
+
 /*
 // 다운로드
 $localDownloadFile = 'C:/Users/chomk/Downloads/image.png';
@@ -47,6 +68,25 @@ if ($sftp->get($remoteDownloadFile, $localDownloadFile)) {
 } else {
     echo "File download failed.\n";
 }
+
+// 파일명을 직접 입력 받는 스타일에 코드
+
+// 다운로드할 파일 입력 받기
+echo "Enter the remote file to download: ";
+$remoteDownloadFile = trim(fgets(STDIN));
+
+// 로컬에 저장할 파일 입력 받기
+echo "Enter the local file path to save: ";
+$localDownloadFile = trim(fgets(STDIN));
+
+// 다운로드
+if ($sftp->get($remoteDownloadFile, $localDownloadFile)) {
+    echo "File downloaded successfully.\n";
+} else {
+    echo "File download failed.\n";
+}
+
+
 */
 // SFTP 연결 닫기
 $sftp->disconnect();
