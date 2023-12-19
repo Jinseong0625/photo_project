a8f8b42fef2828658e0cdbe7aee885313d3a3663
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2023-12-19 12:18:38 +0900

    Update S3Connector.php

--- S3Connector.php
+++ S3Connector.php
@@ -12,8 +12,17 @@ class S3Connector {
 
     private function initializeS3() {
         // 환경 변수에서 AWS 액세스 키 및 시크릿 키 가져오기
-        $awsAccessKey = $_SERVER['AWS_ACCESS_KEY'];
-        $awsSecretKey = $_SERVER['AWS_SECRET_KEY'];
+        $awsAccessKey = getenv('AWS_ACCESS_KEY');
+        $awsSecretKey = getenv('AWS_SECRET_KEY');
+
+        // 값이 설정되어 있는지 확인
+        if ($awsAccessKey === false || $awsSecretKey === false) {
+            // 에러를 로그에 기록하거나 출력
+            error_log("Error: AWS_ACCESS_KEY or AWS_SECRET_KEY is not set.");
+            // 또는 화면에 에러 메시지를 출력할 수 있습니다.
+            // echo "Error: AWS_ACCESS_KEY or AWS_SECRET_KEY is not set.";
+            return null;
+        }
         // AWS SDK를 사용하여 S3 클라이언트 초기화
         $s3 = new S3Client([
             'version'     => 'latest',
