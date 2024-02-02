fc1fc64d3558fd45856aacf4d0ec6e2e5c79d6eb
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2024-02-02 14:31:28 +0900

    Update global_var.php

--- global_var.php
+++ global_var.php
@@ -1,7 +1,7 @@
 <?php 
 
 #define("_HOST_", "13.125.36.197");  //Live
-define("_HOST_", "34.22.101.126");  //Live
+define("_HOST_", "34.64.193.173");  //Live
 define("_USER_", "Newto_1");
 define("_PWD_", "newto123");
 define("_DB_", "photoDB");
